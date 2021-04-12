1377a017920d0bc50092f1df86c6218d47f48ea6
Aaron Boodoo <aaronboodoo@live.com>
2021-04-12 15:17:45 -0400

    JUnit Testing Gradle Update

---
+++ Android/CurameApplication/app/src/androidTest/java/com/example/curameapplication/ImageClassifierTest.java
@@ -0,0 +1,10 @@
+package com.example.curameapplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageClassifierTest {
+
+    @org.junit.jupiter.api.Test
+    void classify() {
+    }
+}
